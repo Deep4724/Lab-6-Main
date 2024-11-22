9df1a3538edb828f040f9d1027e555a42b3dc2ee
Deep Patel <166842823+Deep4724@users.noreply.github.com>
2024-11-21 18:55:17 -0700

    *******

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,3 @@
-/****************** YOUR NAME: Het Rajendra Patel
-
-The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
-
-You are encouraged to use the provided naming convention for ease of review.
-
-*/
 
 
 /****************** create variables ******************/
